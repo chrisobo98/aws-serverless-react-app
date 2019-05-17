8c68d70721014abe24deb25523845923efa848d8
chrisobo98 <christopherbermudez98@gmail.com>
2019-05-17 15:05:56 -0400

    set up backend directories with awsmobilejs. Imported the amplfy API & Components from React. Wrote a request within the ComponentDidMount function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My First React App
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/chrisobo98"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit Github
-        </a>
-      </header>
-    </div>
-  );
-}
+let apiName = 'sampleCloudApi';
+let path = '/items';
+
+class App extends Component {
+
+  componentDidMount(){
+      API.get(apiName, path).then(response => {
+        console.log(response)
+      });
+    }
+  
+  render() {
+    return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to AWS Serveless App made with React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        );
+      }
+    }
 
 export default App;
