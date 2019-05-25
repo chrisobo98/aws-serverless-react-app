308aea6a509b85295b181a13d8a268453872b108
chrisobo98 <christopherbermudez98@gmail.com>
2019-05-25 09:23:48 -0400

    set up the apiName, path, and body of whatis being sent. Using Amplify's API components to post and log the contents to the console.

--- src/screens/createItem.js
+++ src/screens/createItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Modal, Button } from 'semantic-ui-react'
+import Amplify, { API } from 'aws-amplify';
 
 class CreateItemModal extends Component {
 
@@ -7,6 +8,7 @@ constructor(props) {
     super(props)
     this.state = {}
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 handleChange(event, {name, value}) {
@@ -14,6 +16,20 @@ handleChange(event, {name, value}) {
   }
 
 handleSubmit(event) {
+  let apiName = 'sampleCloudApi';
+    let path = '/items';
+    let newItem = {
+      body: {
+          name: this.state.itemName, price: this.state.itemPrice, description: this.state.itemDescription
+        }
+      }
+    API.post(apiName, path, newItem).then(response => {
+    console.log(response)
+    }).catch(error => {
+        console.log(error.response)
+    });
+    event.preventDefault();
+    this.handleClose()
 }
 
 handleOpen = () => this.setState({ modalOpen: true })
@@ -25,12 +41,12 @@ render () {
         <Modal trigger={<Button onClick={this.handleOpen}>+ Add Item</Button>} closeIcon={true} open={this.state.modalOpen} onClose={this.handleClose}>
           <Modal.Header>Add an Item</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Group unstackable widths={2}>
-                <Form.Input name='itemName' label='Item Name' placeholder='Enter Item Name...' onChange={this.handleChange} />
-                <Form.Input name='itemPrice' label='Item Price' placeholder='Enter Item Price...' onChange={this.handleChange} type='number' />
+                <Form.Input name='itemName' label='Item Name' placeholder='Enter Item Name...' onChange={this.handleChange}  value={this.state.itemName} />
+                <Form.Input name='itemPrice' label='Item Price' placeholder='$0.00' onChange={this.handleChange}  value={this.state.itemPrice} />
               </Form.Group>
-              <Form.TextArea name='item_description' label='Item Description' placeholder='Add a Description of the Item...' onChange={this.handleChange} />
+              <Form.TextArea name='item_description' label='Item Description' placeholder='Add a Description of the Item...' onChange={this.handleChange} value={this.state.itemDescription}/>
 
 <Form.Button type='submit'>Submit</Form.Button>
             </Form>
